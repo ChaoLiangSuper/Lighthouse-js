88a9b72cd4a6ff1ac88f99670a64f4b79db74d5b
Chao <cliangsuper@gmail.com>
2020-04-18 17:22:14 +1000

    feat: postgresql connection

--- packages/core/src/db/index.ts
+++ packages/core/src/db/index.ts
@@ -1,27 +1,17 @@
-import mongoose from 'mongoose';
-import { instance } from '../config';
+import { Client } from 'pg';
 
-export const connect = async (): Promise<void> => {
-  const dbHost = process.env.DB_HOST || 'mongodb://localhost:27017/';
-  const dbUser = process.env.DB_USER || '';
-  const dbPass = process.env.DB_PASS || '';
-  const dbName = process.env.DB_NAME || 'lighthouse';
+const client = new Client({
+  user: process.env.DB_USER || 'lighthouse',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'lighthouse',
+  password: process.env.DB_PASS || 'admin'
+});
 
+export const connect = async () => {
   try {
-    await mongoose.connect(dbHost, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      user: dbUser,
-      pass: dbPass,
-      dbName: dbName
-    });
-    console.warn(`[DB]: Connected to database`);
+    await client.connect();
+    console.warn('[DB]: Connected to database.');
   } catch (err) {
     console.warn(`[DB]: Unable to connect to database, ${err}`);
   }
-
-  mongoose.connection.on('error', (err) => {
-    console.warn(`[${instance.name}]: ${err}`);
-  });
 };
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,10 +1,9 @@
+require('dotenv').config({ path: '../../.env' });
+
 import http from 'http';
 import app from './app';
-import dotenv from 'dotenv';
 import { instance } from './config';
 import * as db from './db';
-
-dotenv.config({ path: '../../.env' });
 
 const port = process.env.CORE_PORT || 5000;
 const server = http.createServer(app);
